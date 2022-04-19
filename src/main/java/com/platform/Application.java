9460dfc75890937091a114c8135edd88af81a6c2
JEONSEOEON <seoeon.jeon@e4net.net>
2022-04-19 10:14:44 +0900

    project 이름 변경

--- src/main/java/com/platform/Application.java
+++ src/main/java/com/platform/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PianoPlatformApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PianoPlatformApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
